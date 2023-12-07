956c2181d28d2024f0d5b2c187550f38dd4ddd8c
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-12-07 12:39:45 -0300

    Update app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,27 @@
-document.addEventListener
+const BOARD_WIDTH = 10;
+const BOARD_HEIGHT = 20;
+
+const board = [];
+const bgm = document.createElement('audio');
+const breakSound = document.createElement('audio');
+const drop = document.createElement('audio')
+
+let rotatedShape;
+
+bgm.setAttribute('src', './assets/bgm.mp3');
+bgm.muted = true;
+
+breakSound.setAttribute('src', './assets/break.mp3');
+bgm.muted = true;
+
+breakSound.setAttribute('src', './assets/drop.mp3');
+bgm.muted = true;
+
+for(let row = 0; row < BOARD_HEIGHT; row++){
+    board[row] = [];
+    for(let col =0; col < BOARD_WIDTH; col++){
+        board[row][col] = 0;
+    }
+}
+
+
